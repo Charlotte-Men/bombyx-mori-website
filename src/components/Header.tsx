08706f7313ff56885e4305c7e9b7e7ea418b0770
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2022-09-15 16:11:10 +0200

    Menu / Close button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import header from './Header.module.css';
+import { useState } from 'react';
 import logoBM from '../assets/logo-bombyx-mori.png';
 import logoContact from '../assets/icon-contact.png';
-import iconMenu from '../assets/icon-menu-white.png'
+import header from './Header.module.css';
 
 const Header = () => {
+const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+
   return (
     <div className={header.header__container}>
-      <img src={iconMenu} alt='Menu' className={header.header__menuButton}/>
+      <button className={isMenuVisible ? header.header__closeButton : header.header__menuButton} onClick={() => setIsMenuVisible(!isMenuVisible)} ></button>
       <img src={logoBM} alt='logo Bombyx Mori' className={header.header__logo}/>
       <img src={logoContact} alt='Contact' className={header.header__contactButton}/>
     </div>
