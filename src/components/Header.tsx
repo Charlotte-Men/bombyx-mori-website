36b152b2682bc59d64d552adedd7462ef65c2d1d
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2022-09-15 17:05:54 +0200

    Menu Context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+
 import logoBM from '../assets/logo-bombyx-mori.png';
 import logoContact from '../assets/icon-contact.png';
 import header from './Header.module.css';
+import MenuContext from '../context/menu';
 
 const Header = () => {
-const [isMenuVisible, setIsMenuVisible] = useState<boolean>(false);
+  const { isMenuVisible, setIsMenuVisible } = useContext(MenuContext)
 
   return (
     <div className={header.header__container}>
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MenuContextProvider } from './context/menu';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MenuContextProvider>
+      <App />
+    </MenuContextProvider>
   </React.StrictMode>
 );
 
