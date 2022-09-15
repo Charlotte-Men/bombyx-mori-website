b3460c89875bccbe42d789d33969d0650c8c5a12
Charlotte Menard <charlotte-menard_student2021@wilder.school>
2022-09-15 14:08:06 +0200

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import header from './Header.module.css';
+import logoBM from '../assets/logo-bombyx-mori.png';
+import logoContact from '../assets/logo-contact.png';
+import iconMenu from '../assets/icon-menu-lined.png'
+
+const Header = () => {
+  return (
+    <div className={header.header__container}>
+      <img src={iconMenu} alt='Menu' className={header.header__menuButton}/>
+      <img src={logoBM} alt='logo Bombyx Mori' className={header.header__logo}/>
+      <img src={logoContact} alt='Contact' className={header.header__contactButton}/>
+    </div>
+  );
+}
+
+export default Header;
